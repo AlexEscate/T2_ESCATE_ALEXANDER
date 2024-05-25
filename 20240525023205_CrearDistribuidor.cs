0a70080c717b788762efd4c88680eea50ce4a67f
AlexEscate <escatealex2916@gmail.com>
2024-05-25 06:20:15 -0500

    Agregamos nuevos archivos

---
+++ 20240525023205_CrearDistribuidor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace T2_ESCATE_ALEXANDER.Migrations
+{
+    /// <inheritdoc />
+    public partial class CrearDistribuidor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
